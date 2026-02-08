6af0951a2b97f61f8d5b167754a62a8bb5855965
ariesclark <ariesclark@users.noreply.github.com>
2026-02-08 04:54:15 +0000

    Upgrade Python SDK to spec 1.20.8.dev8

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ Join the [Discord server](https://discord.gg/Ge2APMhPfD) to get in touch with us
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "vrchatapi"
-VERSION = "1.20.8.dev7"
+VERSION = "1.20.8.dev8"
 # To install the library, run the following
 #
 # python setup.py install
--- vrchatapi/models/world.py
+++ vrchatapi/models/world.py
@@ -531,8 +531,8 @@ class World(object):
         if self.local_vars_configuration.client_side_validation and name is None:  # noqa: E501
             raise ValueError("Invalid value for `name`, must not be `None`")  # noqa: E501
         if (self.local_vars_configuration.client_side_validation and
-                name is not None and len(name) < 1):
-            raise ValueError("Invalid value for `name`, length must be greater than or equal to `1`")  # noqa: E501
+                name is not None and len(name) < 0):
+            raise ValueError("Invalid value for `name`, length must be greater than or equal to `0`")  # noqa: E501
 
         self._name = name
 
